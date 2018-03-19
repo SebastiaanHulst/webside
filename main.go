c7b4126c2c66fcebffb9be16cfe5b6c420cb343d
SebastiaanHulst <sebastiaanhulst@gmail.com>
2018-03-19 14:35:50 +0100

    Added test file

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	// Import the gorilla/mux library we just installed
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+func main() {
+	// Declare a new router
+	r := mux.NewRouter()
+
+	// This is where the router is useful, it allows us to declare methods that
+	// this path will be valid for
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	// We can then pass our router (after declaring all our routes) to this method
+	// (where previously, we were leaving the secodn argument as nil)
+	http.ListenAndServe(":8080", r)
 }
 
-func main() {
-	http.HandleFunc("/", helloWorld)
-	http.ListenAndServe(":8080", nil)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
 }
